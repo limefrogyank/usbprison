46c534e52bca3406dedd80cc8511d0490ff09975
Lee McPherson <leemcpherson@hotmail.com>
2026-01-28 16:59:03 -0500

    wtf

--- usbprison.console/IPService.cs
+++ usbprison.console/IPService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Serilog;
 
 namespace usbprison
 {
@@ -10,17 +11,28 @@ namespace usbprison
     {
         public Task<IPAddress?> GetBroadcastAddress()
         {
-            var host = Dns.GetHostEntry(Dns.GetHostName());
-            IPAddress? address = null;
-            foreach (var ip in host.AddressList)
+            string localIP;
+            using (Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0))
             {
-                if (ip.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    address = ip; 
-                    break;
-                }
+                socket.Connect("8.8.8.8", 65530); // Connect to a public IP
+                IPEndPoint endPoint = socket.LocalEndPoint as IPEndPoint;
+                localIP = endPoint.Address.ToString();
             }
+            IPAddress? address = null;
+            var success = IPAddress.TryParse(localIP, out address);
+            // var host = Dns.GetHostEntry(Dns.GetHostName());
+            // IPAddress? address = null;
+            // foreach (var ip in host.AddressList)
+            // {
+            //     if (ip.AddressFamily == AddressFamily.InterNetwork)
+            //     {
+            //         address = ip;
+            //         break;
+            //     }
+            // }
+            Log.Information($"Local IP Address: {address}");
             var broadcast = address?.GetBroadcastAddress(address.GetSubnetMask());
+            Log.Information($"Broadcast IP Address: {broadcast}");
             return Task.FromResult(broadcast);
         }
     }
